5ab8ee18beca4ba5701fba8cce0d2b26a0c31ac4
lauta12 <lautavali@gmail.com>
2024-03-07 23:59:07 -0300

    Added the option to create a password with any numbers of characters you want

--- Admin_Contra.py
+++ Admin_Contra.py
@@ -6,15 +6,7 @@ import string
 import platform
 import pyperclip as cb
 
-#Creo el generador de contraseñas seguras
-letters = string.ascii_letters
-digits = string.digits
-special_chars = string.punctuation
-abc = letters + digits + string.punctuation
-pass_length = 42
-password = ''
-for i in range(pass_length):
-    password += ''.join(secrets.choice(abc))
+
 
 #Obtengo el nombre del sistema operativo
 OS = platform.uname()[0]
@@ -41,16 +33,26 @@ if OS == "Windows":
         pass
 
 #Creo el nombre del archivo
-nombre_archivo = input(str("ingrese nombre del archivo txt: "))
+nombre_archivo = input(str("type the name of the txt file: "))
 
 #Guardo y creo el documento de texto
 ruta_completa = os.path.join(path, f"{nombre_archivo}.txt")
-gmail = input("Ingrese tu gmail, si no hay, apreta enter XD: ")
-Nombre_de_usuario = input("Ingrese su nombre de usuario, si no hay, apreta enter XD: ")
+gmail = input("Type your email: ")
+Nombre_de_usuario = input("Type your username (if its not required press 'enter'): ")
+pass_length = input("Type how many characters your password will have: ")
+
+#Creo el generador de contraseñas seguras
+letters = string.ascii_letters
+digits = string.digits
+special_chars = string.punctuation
+abc = letters + digits + string.punctuation
+password = ''
+
+for i in range(int(pass_length)):
+    password += ''.join(secrets.choice(abc))
 
 with open(ruta_completa, "w") as f:
     f.write(gmail + "\n\n" + Nombre_de_usuario + "\n\n\n" + password)
-
 
 
 # Abro el archivo de texto dependiendo del sistema operativo
@@ -60,7 +62,7 @@ elif OS == "Linux":
     opener = "xdg-open" if os.path.exists("/usr/bin/xdg-open") else "gnome-open"
     os.system(f"{opener} {ruta_completa}")
 else:
-    print("No se pudo determinar el sistema operativo.")
+    print("Your operative system cannot be determined")
 
 #guardo la contraseña al portapapeles
 cb.copy(password)
